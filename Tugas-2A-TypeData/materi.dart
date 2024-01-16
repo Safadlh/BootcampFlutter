68b99a6a0d9c8de6ca4dcb61d05e54d755ecc37a
Safadlh <safa.adilah12@gmail.com>
2024-01-17 04:45:32 +0700

    Pengumpulan Tugas 2A

---
+++ Tugas-2A-TypeData/materi.dart
@@ -0,0 +1,26 @@
+// import 'dart:io';
+// void main(List<String> args) {
+//   print("Masukan password :");
+//   String? inputText = stdin.readLineSync()!;
+//   print("Password : ${inputText}");
+//  }
+
+// void main(List<String> args) {
+//   print(true && true); // true
+//   print(true && false); // false
+//   print(false && false); // false
+//   print(false && true && true); // false
+//   print(true && true && true); // true
+// }
+
+void main() {
+  //  print(num.parse('12')); //12
+  //  print(num.parse('10.91')); //10.91
+
+  //  print(num.parse('12A')); 
+  //  print(num.parse('AAAA')); 
+
+   int j = 45;
+   String t = "$j";  
+   print("hello"+ t);
+}
---
+++ Tugas-2A-TypeData/type.dart
@@ -0,0 +1,57 @@
+//No.1 (Membuat kalimat)
+// void main(){
+//   var word = 'dart';
+//   var second = 'is';
+//   var third = 'awesome';
+//   var fourth = 'and';
+//   var fifth = 'I';
+//   var sixth = 'love';
+//   var seventh = 'it!';
+
+//   print("Output : $word $second $third $fourth $fifth $sixth $seventh");
+// }
+
+//No.2 (Mengurai kalimat)
+// void main(){
+//   var sentence = "I am going to be Flutter Developer";
+//   var exampleFirstWord = sentence[0] ;
+//   var exampleSecondWord = sentence[2] + sentence[3] ;
+//   var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9] ; 
+//   var fourthWord = sentence[11] + sentence[12] ;
+//   var fifthWord = sentence[14] + sentence[15] ; 
+//   var sixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23] ; 
+//   var seventhWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] + sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33];
+
+//   print('First Word: ' + exampleFirstWord);
+//   print('Second Word: ' + exampleSecondWord);
+//   print('Third Word: ' + thirdWord);
+//   print('Fourth Word: ' + fourthWord);
+//   print('Fifth Word: ' + fifthWord);
+//   print('Sixth Word: ' + sixthWord);
+//   print('Seventh Word: ' + seventhWord);
+// }
+
+//No.3 (Menggabungkan nama)
+// import 'dart:io';
+// void main(){
+//   print("Masukan nama depan :");
+//   String? inputText1 = stdin.readLineSync()!;
+//   print("Masukan nama belakang :");
+//   String? inputText2 = stdin.readLineSync()!;
+//   print("Nama lengkap anda adalah : $inputText1 $inputText2");
+// }
+
+//No.4 (Operator)
+// void main(){
+//   int a = 5;
+//   int b = 10;
+//   var kali = a * b ;
+//   var bagi = a / b ;
+//   var tambah = a + b ;
+//   var kurang = a - b ;
+
+//   print("Perkalian : $kali");
+//   print("Pembagian : $bagi");
+//   print("Penambahan : $tambah");
+//   print("Pengurangan : $kurang");
+// }
